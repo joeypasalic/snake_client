c177c6c590880bb443b54ccd52dc8627e18d4ef5
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-14 14:19:20 -0500

    implemented setupInput() & handleUserInput() inside of it, which is sending user inputs to the server

--- play.js
+++ play.js
@@ -1,3 +1,32 @@
 const { connect } = require('./client');
 console.log("Connecting ...");
-connect();
+
+const connection = connect();
+
+const setupInput = function() {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+
+  const handleUserInput = function(key) {
+
+    if (key === '\u0003') {
+      process.exit();
+    }
+
+  };
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+
+};
+
+setupInput();
+
+
+connection.on('close', () => {
+  process.exit(1);
+});
