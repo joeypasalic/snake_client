9f4c8d4ae944897084093e519f5c5ca44887092e
josephpasalic <156459437+joeypasalic@users.noreply.github.com>
2024-02-14 20:48:06 -0500

    implemented custom messages at line 43. works but not well

--- input.js
+++ input.js
@@ -1,3 +1,7 @@
+const readline = require('readline');
+let moving = true;
+
+
 let connection;
 
 const setupInput = (conn) => {
@@ -10,35 +14,55 @@ const setupInput = (conn) => {
   stdin.resume();
 
 
-  const handleUserInput = function(key) {
+  const handleUserInput = function (key) {
 
     if (key === '\u0003') {
       process.exit();
     }
 
     //w key
-    if (key === 'w') {
+    if (key === 'w' && moving) {
       connection.write("Move: up");
     }
 
     //a key
-    if (key === 'a') {
+    if (key === 'a' && moving) {
       connection.write("Move: left");
     }
 
     //s key
-    if (key === 's') {
+    if (key === 's' && moving) {
       connection.write("Move: down");
     }
 
     //d key
-    if (key === 'd') {
+    if (key === 'd' && moving) {
       connection.write("Move: right");
     }
 
+    if (key === ',') {
+
+      moving = false;
+
+      const rl = readline.createInterface({
+
+        input: process.stdin,
+        output: process.stdout
+
+      });
+
+      //implementation of custom messages, works but not well
+      rl.question('Say: ', (input) => {
+        connection.write(`Say: ${input}`);
+        moving = true;
+
+
+      });
+    }
   };
 
   stdin.on("data", handleUserInput);
+
   return stdin;
 
 };
